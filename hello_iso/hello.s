3daca7e1264c379369abcabf00090c941e17d0f6
wangxuan <wangxuan0809@126.com>
2019-05-07 11:56:56 +0800

    hello_iso

---
+++ hello_iso/hello.s
@@ -0,0 +1,20 @@
+	.file	"hello.c"
+	.section	.rodata
+.LC0:
+	.string	"hello"
+	.text
+.globl main
+	.type	main, @function
+main:
+	pushl	%ebp
+	movl	%esp, %ebp
+	andl	$-16, %esp
+	subl	$16, %esp
+	movl	$.LC0, (%esp)
+	call	puts
+	movl	$0, %eax
+	leave
+	ret
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.4.7 20120313 (Red Hat 4.4.7-11)"
+	.section	.note.GNU-stack,"",@progbits
